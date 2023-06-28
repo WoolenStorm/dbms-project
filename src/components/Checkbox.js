d7188724020d6fe2699259ba975ad75680471b8e
Igor Brandes <igorrr.brandes@gmail.com>
2023-06-28 14:02:53 +0200

    created a state object, which shows bike types currently chosen

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,16 +1,14 @@
 import "../styles.css"
 
-export default function Checkbox({ label, isChecked, setChecked }) {
+const Checkbox = ({ label, isChecked, setChecked }) => {
     return (
-        <div className="checkboxContainer" onClick={() => {
-            setChecked((prev) => !prev)
-            console.log(isChecked)
-        }}>
+        <div className="checkboxContainer" onClick={() => setChecked()}>
             <div className="checkboxDiv">
-                <span className={isChecked ? "checked" : "notChecked"} onChange={() => setChecked((prev) => !prev)}></span>
-                {/* <input type="checkbox" checked={isChecked} onChange={() => setChecked((prev) => !prev)} /> */}
+                <span className={isChecked ? "checked" : "notChecked"}></span>
                 <label>{label}</label>
             </div>
         </div>
     )
 }
+
+export default Checkbox
--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,8 +1,21 @@
 import CurrencyInput from "react-currency-input-field";
 import Date from "./Date"
 import Checkbox from "./Checkbox";
+import { useState } from "react";
 
-export default function ControlPanel() {
+const ControlPanel = () => {
+
+    const [isChecked, setChecked] = useState({
+        lastenfahrrad: false,
+        sonstiges: false,
+        fahrrad: false,
+        herrenfahrrad: false,
+        kinderfahrrad: false,
+        mountainbike: false,
+        rennrad: false,
+        damenfahrrad: false
+    })
+
     return (
         <div style={{ paddingLeft: 24 }}>
             <h2 style={{ marginLeft: 16 }}>Datum</h2>
@@ -35,7 +48,77 @@ export default function ControlPanel() {
                 />
             </div>
 
-
+            <h2 style={{ marginLeft: 16 }}>Gewählte Gruppen</h2>
+            <div style={{ display: "flex", flexDirection: "row", padding: 16, marginLeft: 32 }}>
+                <Checkbox
+                    label="Lastenfahrrad"
+                    isChecked={isChecked.lastenfahrrad}
+                    setChecked={() => {
+                        setChecked({ ...isChecked, ...{ lastenfahrrad: !isChecked.lastenfahrrad } })
+                    }}
+                />
+                <div style={{ width: 64 }} />
+                <Checkbox
+                    label="Kinderfahrrad"
+                    isChecked={isChecked.kinderfahrrad}
+                    setChecked={() => {
+                        setChecked({ ...isChecked, ...{ kinderfahrrad: !isChecked.kinderfahrrad } })
+                    }}
+                />
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", padding: 16, marginLeft: 32 }}>
+                <Checkbox
+                    label="Damenfahrrad"
+                    isChecked={isChecked.damenfahrrad}
+                    setChecked={() => {
+                        setChecked({ ...isChecked, ...{ damenfahrrad: !isChecked.damenfahrrad } })
+                    }}
+                />
+                <div style={{ width: 64 }} />
+                <Checkbox
+                    label="Mountainbike"
+                    isChecked={isChecked.mountainbike}
+                    setChecked={() => {
+                        setChecked({ ...isChecked, ...{ mountainbike: !isChecked.mountainbike } })
+                    }}
+                />
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", padding: 16, marginLeft: 32 }}>
+                <Checkbox
+                    label="Herrenfahrrad"
+                    isChecked={isChecked.herrenfahrrad}
+                    setChecked={() => {
+                        setChecked({ ...isChecked, ...{ herrenfahrrad: !isChecked.herrenfahrrad } })
+                    }}
+                />
+                <div style={{ width: 64 }} />
+                <Checkbox
+                    label="Rennrad"
+                    isChecked={isChecked.rennrad}
+                    setChecked={() => {
+                        setChecked({ ...isChecked, ...{ rennrad: !isChecked.rennrad } })
+                    }}
+                />
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", padding: 16, marginLeft: 32 }}>
+                <Checkbox
+                    label="Fahrrad (?)"
+                    isChecked={isChecked.fahrrad}
+                    setChecked={() => {
+                        setChecked({ ...isChecked, ...{ fahrrad: !isChecked.fahrrad } })
+                    }}
+                />
+                <div style={{ width: 64 }} />
+                <Checkbox
+                    label="Sonstiges"
+                    isChecked={isChecked.sonstiges}
+                    setChecked={() => {
+                        setChecked({ ...isChecked, ...{ sonstiges: !isChecked.sonstiges } })
+                    }}
+                />
+            </div>
         </div>
     )
 }
+
+export default ControlPanel
