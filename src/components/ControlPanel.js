5c68c8a9679ff19554b66c3e1dc72655bf1c1181
Igor Brandes <igorrr.brandes@gmail.com>
2023-07-06 20:03:27 +0200

    added some switches (to disable kriteria alltogether)

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,7 +1,8 @@
 import CurrencyInput from "react-currency-input-field";
 import Date from "./Date"
 import Checkbox from "./Checkbox";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Toggle from "react-styled-toggle";
 
 const ControlPanel = ({
     chosenBikes, setChosenBikes,
@@ -19,17 +20,46 @@ const ControlPanel = ({
         [chosenBikes, startDate, endDate, minDamage, maxDamage, filterThefts, setTheftsToShow]
     )
 
+    const [isDateEnabled, setIsDateEnabled] = useState(true)
+    const [isDamageEnabled, setIsDamageEnabled] = useState(true)
+    const [isTypeEnabled, setIsTypeEnabled] = useState(true)
+
     return (
         <div style={{ paddingLeft: 24 }}>
-            <h2 style={{ marginLeft: 16 }}>Datum</h2>
 
-            <div style={{ display: "flex", flexDirection: "row" }}>
-                <Date placeholder="von" value={startDate} setValue={setStartDate} disabled={!enabled} />
-                <div style={{ width: 64 }} />
-                <Date placeholder="bis" value={endDate} setValue={setEndDate} disabled={!enabled} />
+            <div style={{ display: "flex", flexDirection: "row", position: "relative" }}>
+
+                <div className="switchContainer">
+                    <Toggle checked={isDateEnabled} onChange={() => setIsDateEnabled(!isDateEnabled)}
+                        width={53} height={25} sliderWidth={20} sliderHeight={20}
+                        backgroundColorChecked="#49967F" backgroundColorUnchecked="#3B3955" backgroundColorButton="#36FCC0"
+                    />
+                </div>
+
+                <h2 style={{ marginLeft: 64 }}>Datum</h2>
             </div>
 
-            <h2 style={{ marginLeft: 16 }}>Schadenshöhe</h2>
+
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <Date placeholder="von" value={startDate} setValue={setStartDate} disabled={!enabled || !isDateEnabled} />
+                <div style={{ width: 64 }} />
+                <Date placeholder="bis" value={endDate} setValue={setEndDate} disabled={!enabled || !isDateEnabled} />
+            </div>
+
+            <div style={{ display: "flex", flexDirection: "row", position: "relative" }}>
+
+                <div className="switchContainer">
+                    <Toggle
+                        checked={isDamageEnabled} onChange={() => setIsDamageEnabled(!isDamageEnabled)}
+                        width={53} height={25} sliderWidth={20} sliderHeight={20}
+                        backgroundColorChecked="#49967F" backgroundColorUnchecked="#3B3955" backgroundColorButton="#36FCC0"
+                    />
+                </div>
+
+                <h2 style={{ marginLeft: 64 }}>Schadenshöhe</h2>
+            </div>
+
+
 
             <div style={{ display: "flex", flexDirection: "row" }}>
                 <CurrencyInput
@@ -38,7 +68,7 @@ const ControlPanel = ({
                     defaultValue={minDamage}
                     decimalsLimit={0}
                     prefix="€"
-                    disabled={!enabled}
+                    disabled={!enabled || !isDamageEnabled}
                     onValueChange={(value, _) => {
                         setMinDamage(value)
                     }}
@@ -50,85 +80,96 @@ const ControlPanel = ({
                     defaultValue={maxDamage}
                     decimalsLimit={0}
                     prefix="€"
-                    disabled={!enabled}
+                    disabled={!enabled || !isDamageEnabled}
                     onValueChange={(value, _) => {
                         setMaxDamage(value)
                     }}
                 />
             </div>
 
-            <h2 style={{ marginLeft: 16 }}>Gewählte Gruppen</h2>
-            <div style={{ display: "flex", flexDirection: "row", padding: 16, marginLeft: 32 }}>
+            <div style={{ display: "flex", flexDirection: "row", position: "relative" }}>
+
+                <div className="switchContainer">
+                    <Toggle
+                        checked={isTypeEnabled} onChange={() => setIsTypeEnabled(!isTypeEnabled)}
+                        width={53} height={25} sliderWidth={20} sliderHeight={20}
+                        backgroundColorChecked="#49967F" backgroundColorUnchecked="#3B3955" backgroundColorButton="#36FCC0"
+                    />
+                </div>
+
+                <h2 style={{ marginLeft: 64 }}>Gewählte Gruppen</h2>
+            </div>
+            <div style={{ display: "flex", flexDirection: "row", padding: 16 }}>
                 <Checkbox
                     label="Lastenfahrrad"
                     isChecked={chosenBikes.lastenfahrrad}
-                    disabled={!enabled}
+                    disabled={!enabled || !isTypeEnabled}
                     setChecked={() => {
                         setChosenBikes({ ...chosenBikes, ...{ lastenfahrrad: !chosenBikes.lastenfahrrad } })
                     }}
                 />
-                <div style={{ width: 64 }} />
+                <div style={{ width: 98 }} />
                 <Checkbox
                     label="Kinderfahrrad"
                     isChecked={chosenBikes.kinderfahrrad}
-                    disabled={!enabled}
+                    disabled={!enabled || !isTypeEnabled}
                     setChecked={() => {
                         setChosenBikes({ ...chosenBikes, ...{ kinderfahrrad: !chosenBikes.kinderfahrrad } })
                     }}
                 />
             </div>
-            <div style={{ display: "flex", flexDirection: "row", padding: 16, marginLeft: 32 }}>
+            <div style={{ display: "flex", flexDirection: "row", padding: 16 }}>
                 <Checkbox
                     label="Damenfahrrad"
                     isChecked={chosenBikes.damenfahrrad}
-                    disabled={!enabled}
+                    disabled={!enabled || !isTypeEnabled}
                     setChecked={() => {
                         setChosenBikes({ ...chosenBikes, ...{ damenfahrrad: !chosenBikes.damenfahrrad } })
                     }}
                 />
-                <div style={{ width: 64 }} />
+                <div style={{ width: 98 }} />
                 <Checkbox
                     label="Mountainbike"
                     isChecked={chosenBikes.mountainbike}
-                    disabled={!enabled}
+                    disabled={!enabled || !isTypeEnabled}
                     setChecked={() => {
                         setChosenBikes({ ...chosenBikes, ...{ mountainbike: !chosenBikes.mountainbike } })
                     }}
                 />
             </div>
-            <div style={{ display: "flex", flexDirection: "row", padding: 16, marginLeft: 32 }}>
+            <div style={{ display: "flex", flexDirection: "row", padding: 16 }}>
                 <Checkbox
                     label="Herrenfahrrad"
                     isChecked={chosenBikes.herrenfahrrad}
-                    disabled={!enabled}
+                    disabled={!enabled || !isTypeEnabled}
                     setChecked={() => {
                         setChosenBikes({ ...chosenBikes, ...{ herrenfahrrad: !chosenBikes.herrenfahrrad } })
                     }}
                 />
-                <div style={{ width: 64 }} />
+                <div style={{ width: 98 }} />
                 <Checkbox
                     label="Rennrad"
                     isChecked={chosenBikes.rennrad}
-                    disabled={!enabled}
+                    disabled={!enabled || !isTypeEnabled}
                     setChecked={() => {
                         setChosenBikes({ ...chosenBikes, ...{ rennrad: !chosenBikes.rennrad } })
                     }}
                 />
             </div>
-            <div style={{ display: "flex", flexDirection: "row", padding: 16, marginLeft: 32 }}>
+            <div style={{ display: "flex", flexDirection: "row", padding: 16 }}>
                 <Checkbox
                     label="Fahrrad (?)"
                     isChecked={chosenBikes.fahrrad}
-                    disabled={!enabled}
+                    disabled={!enabled || !isTypeEnabled}
                     setChecked={() => {
                         setChosenBikes({ ...chosenBikes, ...{ fahrrad: !chosenBikes.fahrrad } })
                     }}
                 />
-                <div style={{ width: 64 }} />
+                <div style={{ width: 98 }} />
                 <Checkbox
                     label="Sonstiges"
                     isChecked={chosenBikes.sonstiges}
-                    disabled={!enabled}
+                    disabled={!enabled || !isTypeEnabled}
                     setChecked={() => {
                         setChosenBikes({ ...chosenBikes, ...{ sonstiges: !chosenBikes.sonstiges } })
                     }}
